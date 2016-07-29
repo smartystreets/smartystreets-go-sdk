5ee9365f1633ba5d64036f5d4b868b00e947062b
Whatcott, Michael <mdwhatcott@gmail.com>
2016-07-29 14:59:59 -0600

    Builder panics instead of returning error. Supports specification of retry and timeout values.

--- us-street-api/wireup.go
+++ us-street-api/wireup.go
@@ -4,6 +4,9 @@ import (
 	"net/http"
 	"net/url"
 
+	"fmt"
+	"time"
+
 	"bitbucket.org/smartystreets/smartystreets-go-sdk"
 	"bitbucket.org/smartystreets/smartystreets-go-sdk/internal/sdk"
 )
@@ -13,13 +16,15 @@ import (
 type ClientBuilder struct {
 	credential smarty_sdk.Credential
 	baseURL    string
-	err        error
+	retries    int
+	timeout    time.Duration
 }
 
 // NewClientBuilder creates a new client builder, ready to receive calls to its chain-able methods.
 func NewClientBuilder() *ClientBuilder {
 	return &ClientBuilder{
 		credential: &sdk.NopCredential{},
+		timeout:    time.Second * 10,
 	}
 }
 
@@ -33,18 +38,38 @@ func (b *ClientBuilder) WithSecretKeyCredential(authID, authToken string) *Clien
 // WithSecretKeyCredential allows the caller to specify the url that the client will use.
 // In all but very few use cases the default value is sufficient and this method should not be called.
 func (b *ClientBuilder) WithCustomBaseURL(uri string) *ClientBuilder {
-	_, b.err = url.Parse(uri)
+	_, err := url.Parse(uri)
+	if err != nil {
+		panic(fmt.Sprint("Could not parse provided address:", err.Error()))
+	}
 	b.baseURL = uri
 	return b
 }
 
-// Builds the client using the provided configuration details provided by other methods on the ClientBuilder.
-func (b *ClientBuilder) Build() (*Client, error) {
-	if b.err != nil {
-		return nil, b.err
+// WithMaxRetry allows the caller to specify the number of times an API request will be resent in the
+// case of network errors or unexpected results.
+func (b *ClientBuilder) WithMaxRetry(retries int) *ClientBuilder {
+	if retries < 0 {
+		panic(fmt.Sprintf("Please provide a non-negative number of retry attempts (you supplied %d).", retries))
 	}
+	b.retries = retries
+	return b
+}
 
-	signingClient := sdk.NewSigningClient(http.DefaultClient, b.credential)
+// WithTimeout allows the caller to specify the timeout for all API requests.
+func (b *ClientBuilder) WithTimeout(duration time.Duration) *ClientBuilder {
+	if duration < 0 {
+		panic(fmt.Sprintf("Please provide a non-negative duration (you supplied %s).", duration.String()))
+	}
+	b.timeout = duration
+	return b
+}
+
+// Builds the client using the provided configuration details provided by other methods on the ClientBuilder.
+func (b *ClientBuilder) Build() *Client {
+	client := http.Client{Timeout: b.timeout}
+	retryClient := sdk.NewRetryClient(client, b.retries)
+	signingClient := sdk.NewSigningClient(retryClient, b.credential)
 	sender := sdk.NewHTTPSender(signingClient)
-	return NewClient(sender), nil
+	return NewClient(sender)
 }
