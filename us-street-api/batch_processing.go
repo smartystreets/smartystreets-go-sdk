4075025870440bcc31826a17141369c8f9683ccb
Whatcott, Michael <mdwhatcott@gmail.com>
2016-08-05 17:01:05 -0600

    The batch processor now sends inputs on an output channel as they finish processing.

--- us-street-api/batch_processing.go
+++ us-street-api/batch_processing.go
@@ -4,47 +4,58 @@ package street
 // to send all lookups provided in serial, blocking fashion.
 func (c *Client) SendLookups(lookups ...*Lookup) error {
 	stream := make(chan *Lookup)
-	go load(stream, lookups)
-	return c.SendFromChannel(stream)
+	go loadAll(stream, lookups)
+	return c.SendFromChannel(stream, nil)
+}
+
+func loadAll(stream chan *Lookup, lookups []*Lookup) {
+	load(stream, lookups)
+	close(stream)
 }
 
 func load(stream chan *Lookup, lookups []*Lookup) {
 	for _, lookup := range lookups {
 		stream <- lookup
 	}
-	close(stream)
 }
 
-// SendFromChannel is a high-level convenience function that leverages an internal reusable Batch
-// to send everything received from the provided lookups channel in serial, blocking fashion.
-func (c *Client) SendFromChannel(lookups <-chan *Lookup) error {
-	processor := newBatchProcessor(lookups, c)
+// SendFromChannel is a high-level convenience function that leverages an
+// internal reusable Batch to send everything received from the provided
+// input channel in serial, blocking fashion. The same lookups are sent
+// on the output channel as they are processed. Whereas it is the caller's
+// job to close the input channel when all inputs have been sent, the output
+// channel will be closed for the caller.
+func (c *Client) SendFromChannel(input, output chan *Lookup) error {
+	processor := newBatchProcessor(input, output, c)
 	processor.ProcessAll()
 	return processor.Error()
 }
 
 type batchProcessor struct {
-	lookups <-chan *Lookup
-	client  *Client
-	batch   *Batch
-	err     error
+	input  chan *Lookup
+	output chan *Lookup
+	client *Client
+	batch  *Batch
+	err    error
 }
 
-func newBatchProcessor(lookups <-chan *Lookup, client *Client) *batchProcessor {
+func newBatchProcessor(input, output chan *Lookup, client *Client) *batchProcessor {
 	return &batchProcessor{
-		lookups: lookups,
-		client:  client,
-		batch:   NewBatch(),
+		input:  input,
+		output: output,
+		client: client,
+		batch:  NewBatch(),
 	}
 }
 
 func (c *batchProcessor) ProcessAll() {
 	c.readLookups()
 	c.sendLastBatch()
+	c.cleanup()
 }
 
 func (c *batchProcessor) readLookups() {
-	for lookup := range c.lookups {
+	for lookup := range c.input {
 		c.processLookup(lookup)
 		if c.errorOccurred() {
 			break
@@ -61,20 +72,31 @@ func (c *batchProcessor) processLookup(lookup *Lookup) {
 
 func (c *batchProcessor) sendBatch() {
 	c.err = c.client.SendBatch(c.batch)
+	c.routeOutput()
 	c.batch.Clear()
 }
 
-func (c *batchProcessor) sendLastBatch() {
-	if c.errorOccurred() || c.batch.isEmpty() {
-		return
+func (c *batchProcessor) routeOutput() {
+	if c.output != nil {
+		load(c.output, c.batch.Records())
 	}
-	c.sendBatch()
+}
+
+func (c *batchProcessor) sendLastBatch() {
+	if !c.errorOccurred() && !c.batch.isEmpty() {
+		c.sendBatch()
+	}
 }
 
 func (c *batchProcessor) errorOccurred() bool {
 	return c.err != nil
 }
 
+func (c *batchProcessor) cleanup() {
+	if c.output != nil {
+		close(c.output)
+	}
+}
 func (c *batchProcessor) Error() error {
 	return c.err
 }
