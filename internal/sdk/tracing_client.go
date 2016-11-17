55a5363ca3d3eba802b8e395732a918907460e60
Whatcott, Michael <mdwhatcott@gmail.com>
2016-11-17 15:17:53 -0700

    Buffering all log statements.

--- internal/sdk/tracing_client.go
+++ internal/sdk/tracing_client.go
@@ -3,7 +3,6 @@ package sdk
 import (
 	"bytes"
 	"fmt"
-	"io"
 	"net/http"
 	"net/http/httptrace"
 
@@ -25,15 +24,13 @@ func NewTracingClient(inner HTTPClient, trace bool) HTTPClient {
 }
 
 func (d *TracingClient) Do(request *http.Request) (*http.Response, error) {
-	output := new(bytes.Buffer)
-	trace := NewHTTPTrace()
-	trace.SetLogOutput(output)
+	trace := NewHTTPTraceLogger()
 	request = trace.AttachToRequest(request)
 	response, err := d.inner.Do(request)
 	trace.DumpResponse(response, err)
 
 	if err != nil || response.StatusCode != http.StatusOK {
-		d.logger.Println("\n" + output.String())
+		d.logger.Println("\n" + trace.String())
 	}
 
 	return response, err
@@ -41,13 +38,13 @@ func (d *TracingClient) Do(request *http.Request) (*http.Response, error) {
 
 /**************************************************************************/
 
-type HTTPTrace struct {
-	trace  *httptrace.ClientTrace
-	logger *logging.Logger
+type HTTPTraceLogger struct {
+	*bytes.Buffer
+	trace *httptrace.ClientTrace
 }
 
-func NewHTTPTrace() *HTTPTrace {
-	trace := new(HTTPTrace)
+func NewHTTPTraceLogger() *HTTPTraceLogger {
+	trace := &HTTPTraceLogger{Buffer: new(bytes.Buffer)}
 	trace.trace = &httptrace.ClientTrace{
 		ConnectDone:          trace.ConnectDone,
 		ConnectStart:         trace.ConnectStart,
@@ -65,68 +62,64 @@ func NewHTTPTrace() *HTTPTrace {
 	return trace
 }
 
-func (t *HTTPTrace) SetLogOutput(writer io.Writer) {
-	t.logger.SetOutput(writer)
-}
-
-func (t *HTTPTrace) AttachToRequest(request *http.Request) *http.Request {
-	t.logger.Println(dumpRequest(request))
+func (t *HTTPTraceLogger) AttachToRequest(request *http.Request) *http.Request {
+	fmt.Fprintln(t, dumpRequest(request))
 	return request.WithContext(httptrace.WithClientTrace(request.Context(), t.trace))
 }
 
-func (t *HTTPTrace) DumpResponse(response *http.Response, err error) {
-	t.logger.Println(dumpResponse(response, err))
+func (t *HTTPTraceLogger) DumpResponse(response *http.Response, err error) {
+	fmt.Fprintln(t, dumpResponse(response, err))
 }
 
-func (t *HTTPTrace) GetConn(hostPort string) {
-	t.logger.Println("GetConn hostPort:", hostPort)
+func (t *HTTPTraceLogger) GetConn(hostPort string) {
+	fmt.Fprintln(t, "GetConn hostPort:", hostPort)
 }
 
-func (t *HTTPTrace) GotConn(info httptrace.GotConnInfo) {
-	t.logger.Printf("GotConn info: Conn: %s %#v\n", info.Conn.RemoteAddr(), info)
+func (t *HTTPTraceLogger) GotConn(info httptrace.GotConnInfo) {
+	fmt.Fprintf(t, "GotConn info: Conn: %s %#v\n", info.Conn.RemoteAddr(), info)
 }
 
-func (t *HTTPTrace) PutIdleConn(err error) {
-	t.logger.Println("PutIdleConn err:", err)
+func (t *HTTPTraceLogger) PutIdleConn(err error) {
+	fmt.Fprintln(t, "PutIdleConn err:", err)
 }
 
-func (t *HTTPTrace) GotFirstResponseByte() {
-	t.logger.Println("GotFirstResponseByte")
+func (t *HTTPTraceLogger) GotFirstResponseByte() {
+	fmt.Fprintln(t, "GotFirstResponseByte")
 }
 
-func (t *HTTPTrace) Got100Continue() {
-	t.logger.Println("Got100Continue")
+func (t *HTTPTraceLogger) Got100Continue() {
+	fmt.Fprintln(t, "Got100Continue")
 }
 
-func (t *HTTPTrace) DNSStart(info httptrace.DNSStartInfo) {
-	t.logger.Printf("DNSStart info: %#v\n", info)
+func (t *HTTPTraceLogger) DNSStart(info httptrace.DNSStartInfo) {
+	fmt.Fprintf(t, "DNSStart info: %#v\n", info)
 }
 
-func (t *HTTPTrace) DNSDone(info httptrace.DNSDoneInfo) {
+func (t *HTTPTraceLogger) DNSDone(info httptrace.DNSDoneInfo) {
 	addresses := new(bytes.Buffer)
 	for i, address := range info.Addrs {
 		fmt.Fprintf(addresses, "  %d. %s\n", i, address.String())
 	}
-	t.logger.Printf("DNSDone info: Coalesced: %t Err: %v Addresses:\n%s",
+	fmt.Fprintf(t, "DNSDone info: Coalesced: %t Err: %v Addresses:\n%s",
 		info.Coalesced, info.Err, addresses.String())
 }
 
-func (t *HTTPTrace) ConnectStart(network, address string) {
-	t.logger.Println("ConnectStart network:", network, "address:", address)
+func (t *HTTPTraceLogger) ConnectStart(network, address string) {
+	fmt.Fprintln(t, "ConnectStart network:", network, "address:", address)
 }
 
-func (t *HTTPTrace) ConnectDone(network, address string, err error) {
-	t.logger.Println("ConnectDone network:", network, "address:", address, "error:", err)
+func (t *HTTPTraceLogger) ConnectDone(network, address string, err error) {
+	fmt.Fprintln(t, "ConnectDone network:", network, "address:", address, "error:", err)
 }
 
-func (t *HTTPTrace) WroteHeaders() {
-	t.logger.Println("WroteHeaders")
+func (t *HTTPTraceLogger) WroteHeaders() {
+	fmt.Fprintln(t, "WroteHeaders")
 }
 
-func (t *HTTPTrace) Wait100Continue() {
-	t.logger.Println("Wait100Continue")
+func (t *HTTPTraceLogger) Wait100Continue() {
+	fmt.Fprintln(t, "Wait100Continue")
 }
 
-func (t *HTTPTrace) WroteRequest(info httptrace.WroteRequestInfo) {
-	t.logger.Printf("WroteRequest info: %#v\n", info)
+func (t *HTTPTraceLogger) WroteRequest(info httptrace.WroteRequestInfo) {
+	fmt.Fprintf(t, "WroteRequest info: %#v\n", info)
 }
