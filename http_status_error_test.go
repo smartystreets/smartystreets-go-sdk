14d456ec2431e64607cb60f49ab44195767594c5
Whatcott, Michael <mdwhatcott@gmail.com>
2017-03-22 16:15:09 -0600

    Include body of response in non-200 scenarios.

--- http_status_error_test.go
+++ http_status_error_test.go
@@ -14,7 +14,7 @@ func TestHTTPStatusError(t *testing.T) {
 	err := NewHTTPStatusError(http.StatusTeapot, []byte("Hello, World!"))
 
 	assert := assertions.New(t)
-	assert.So(err.Error(), should.Equal, "HTTP 418 I'm a teapot")
+	assert.So(err.Error(), should.Equal, "HTTP 418 I'm a teapot\nHello, World!")
 	assert.So(err.StatusCode(), should.Equal, http.StatusTeapot)
 	assert.So(err.Content(), should.Resemble, []byte("Hello, World!"))
 }
