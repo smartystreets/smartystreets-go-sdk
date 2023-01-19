30765799339186a1e19df648b3b629c382f8bc1c
Ryan Cox <ryan@smarty.com>
2023-01-18 17:19:05 -0700

    Add new(ish) fields to the International Autocomplete Api

--- international-autocomplete-api/lookup.go
+++ international-autocomplete-api/lookup.go
@@ -1,22 +1,41 @@
 package international_autocomplete_api
 
-import "net/url"
+import (
+	"math"
+	"net/url"
+	"strconv"
+)
+
+const (
+	maxResultsDefault = 5
+	distanceDefault   = 5
+)
 
 type Lookup struct {
 	Country            string
 	Search             string
+	MaxResults         int
+	Distance           int
+	Geolocation        InternationalGeolocateType
 	AdministrativeArea string
 	Locality           string
 	PostalCode         string
+	Latitude           float64
+	Longitude          float64
 	Result             *Result
 }
 
 func (l Lookup) populate(query url.Values) {
 	l.populateCountry(query)
 	l.populateSearch(query)
+	l.populateMaxResults(query)
+	l.populateDistance(query)
+	l.populateGeolocation(query)
 	l.populateAdministrativeArea(query)
 	l.populateLocality(query)
 	l.populatePostalCode(query)
+	l.populateLatitude(query)
+	l.populateLongitude(query)
 }
 func (l Lookup) populateCountry(query url.Values) {
 	if len(l.Country) > 0 {
@@ -26,6 +45,27 @@ func (l Lookup) populateCountry(query url.Values) {
 func (l Lookup) populateSearch(query url.Values) {
 	if len(l.Search) > 0 {
 		query.Set("search", l.Search)
+	}
+}
+func (l Lookup) populateMaxResults(query url.Values) {
+	maxResults := l.MaxResults
+	if maxResults < 1 {
+		maxResults = maxResultsDefault
+	}
+	query.Set("max_results", strconv.Itoa(maxResults))
+}
+func (l Lookup) populateDistance(query url.Values) {
+	distance := l.Distance
+	if distance < 1 {
+		distance = distanceDefault
+	}
+	query.Set("distance", strconv.Itoa(distance))
+}
+func (l Lookup) populateGeolocation(query url.Values) {
+	if l.Geolocation != None {
+		query.Set("geolocation", string(l.Geolocation))
+	} else {
+		query.Del("geolocation")
 	}
 }
 func (l Lookup) populateAdministrativeArea(query url.Values) {
@@ -43,3 +83,23 @@ func (l Lookup) populatePostalCode(query url.Values) {
 		query.Set("include_only_postal_code", l.PostalCode)
 	}
 }
+func (l Lookup) populateLatitude(query url.Values) {
+	if math.Floor(l.Latitude) != 0 {
+		query.Set("latitude", strconv.FormatFloat(l.Latitude, 'f', 8, 64))
+	}
+}
+func (l Lookup) populateLongitude(query url.Values) {
+	if math.Floor(l.Longitude) != 0 {
+		query.Set("longitude", strconv.FormatFloat(l.Longitude, 'f', 8, 64))
+	}
+}
+
+type InternationalGeolocateType string
+
+const (
+	AdminArea  = InternationalGeolocateType("adminarea")
+	Locality   = InternationalGeolocateType("locality")
+	PostalCode = InternationalGeolocateType("postalcode")
+	Geocodes   = InternationalGeolocateType("geocodes")
+	None       = InternationalGeolocateType("")
+)
