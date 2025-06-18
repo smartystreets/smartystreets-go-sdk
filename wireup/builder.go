b478d6ffaf9a2560aaf01b8f7a78e2ae871faf4e
Carter Gividen <carter.gividen@smarty.com>
2025-06-18 11:36:56 -0600

    Remove deprecated us-autocomplete.

--- wireup/builder.go
+++ wireup/builder.go
@@ -13,7 +13,6 @@ import (
 	internal "github.com/smartystreets/smartystreets-go-sdk/internal/sdk"
 	international_autocomplete "github.com/smartystreets/smartystreets-go-sdk/international-autocomplete-api"
 	international_street "github.com/smartystreets/smartystreets-go-sdk/international-street-api"
-	"github.com/smartystreets/smartystreets-go-sdk/us-autocomplete-api"
 	autocomplete_pro "github.com/smartystreets/smartystreets-go-sdk/us-autocomplete-pro-api"
 	us_enrichment "github.com/smartystreets/smartystreets-go-sdk/us-enrichment-api"
 	"github.com/smartystreets/smartystreets-go-sdk/us-extract-api"
@@ -139,11 +138,6 @@ func (b *clientBuilder) buildUSZIPCodeAPIClient() *zipcode.Client {
 	return zipcode.NewClient(b.buildHTTPSender())
 }
 
-func (b *clientBuilder) buildUSAutocompleteAPIClient() *autocomplete.Client {
-	b.ensureBaseURLNotNil(defaultBaseURL_USAutocompleteAPI)
-	return autocomplete.NewClient(b.buildHTTPSender())
-}
-
 func (b *clientBuilder) buildUSAutocompleteProAPIClient() *autocomplete_pro.Client {
 	b.ensureBaseURLNotNil(defaultBaseURL_USAutocompleteProAPI)
 	return autocomplete_pro.NewClient(b.buildHTTPSender())
@@ -232,7 +226,6 @@ var (
 	defaultBaseURL_InternationalAutocompleteAPI = &url.URL{Scheme: "https", Host: "international-autocomplete.api.smarty.com"}
 	defaultBaseURL_USStreetAPI                  = &url.URL{Scheme: "https", Host: "us-street.api.smarty.com"}
 	defaultBaseURL_USZIPCodeAPI                 = &url.URL{Scheme: "https", Host: "us-zipcode.api.smarty.com"}
-	defaultBaseURL_USAutocompleteAPI            = &url.URL{Scheme: "https", Host: "us-autocomplete.api.smarty.com"}
 	defaultBaseURL_USEnrichmentAPI              = &url.URL{Scheme: "https", Host: "us-enrichment.api.smarty.com"}
 	defaultBaseURL_USExtractAPI                 = &url.URL{Scheme: "https", Host: "us-extract.api.smarty.com"}
 	defaultBaseURL_USReverseGeocodingAPI        = &url.URL{Scheme: "https", Host: "us-reverse-geo.api.smarty.com"}
