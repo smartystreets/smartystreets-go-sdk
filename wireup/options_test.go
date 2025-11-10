8e8441bb3a9afbe3e085e16776786ab9bf910bd5
Andrew Johnson <andrewjohnson0613@gmail.com>
2025-11-10 14:27:08 -0700

    Fixed custom comma separated query option, added tests.

--- wireup/options_test.go
+++ wireup/options_test.go
@@ -18,3 +18,43 @@ type OptionsFixture struct {
 func (this *OptionsFixture) TestConfigure_NilOptionIgnored() {
 	this.So(func() { configure(nil) }, should.NotPanic)
 }
+
+func (this *OptionsFixture) TestConfigure_WithCustomCommaSeparatedQuery() {
+	builder := configure(
+		WithCustomCommaSeparatedQuery("test", "first"),
+		WithCustomCommaSeparatedQuery("test", "second"),
+		WithCustomCommaSeparatedQuery("test", "third"),
+	)
+	this.So(builder.customQueries.Get("test"), should.Equal, "first,second,third")
+}
+
+func (this *OptionsFixture) TestConfigure_WithFeatureComponentAnalysis() {
+	builder := configure(
+		WithFeatureComponentAnalysis(),
+	)
+	this.So(builder.customQueries.Get("features"), should.Equal, "component-analysis")
+}
+
+func (this *OptionsFixture) TestConfigure_WithFeatureComponentAnalysisAndCustom_ShouldAppend() {
+	builder := configure(
+		WithFeatureComponentAnalysis(),
+		WithCustomCommaSeparatedQuery("features", "test-feature"),
+	)
+	this.So(builder.customQueries.Get("features"), should.Equal, "component-analysis,test-feature")
+}
+
+func (this *OptionsFixture) TestConfigure_WithCustomQuery() {
+	builder := configure(
+		WithCustomQuery("test", "first"),
+	)
+	this.So(builder.customQueries.Get("test"), should.Equal, "first")
+}
+
+func (this *OptionsFixture) TestConfigure_WithCustomQuery_ShouldOverwrite() {
+	builder := configure(
+		WithCustomQuery("test", "first"),
+		WithCustomQuery("test", "second"),
+		WithCustomQuery("test", "third"),
+	)
+	this.So(builder.customQueries.Get("test"), should.Equal, "third")
+}
